329466ad6dcb29661007ba58f9df11383ccf90af
Muss24 <abdulmustopa2004@gmail.com>
2026-08-11 16:06:20 +0700

    update CORS

--- api/get_materials.php
+++ api/get_materials.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://localhost:5173");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
--- api/scan_absen.php
+++ api/scan_absen.php
@@ -1,6 +1,6 @@
 <?php
 // 1. Izinkan CORS dari React secara eksplisit
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://localhost:5173");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
